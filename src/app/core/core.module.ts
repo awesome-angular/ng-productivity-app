d0d42cf567d6af2637bb66e9cc26245c6fb52bad
Simon Dieny <simon.dieny@gmail.com>
2018-12-29 17:26:25 +0100

    fix(core): prevent reload of core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { PublicModule } from '../public/public.module';
@@ -13,4 +13,10 @@ import { ProtectedModule } from '../protected/protected.module';
 		ProtectedModule
 	]
 })
-export class CoreModule { }
+export class CoreModule {
+	constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+		if (parentModule) {
+			throw new Error('CoreModule is already loaded.');
+		}
+	}
+}
