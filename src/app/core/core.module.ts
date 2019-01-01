958f3659baa396f2941997f69b248cd31f97af84
Simon Dieny <simon.dieny@gmail.com>
2019-01-01 16:06:21 +0100

    feat(firebase): integration of @angular/fire

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,21 @@
-import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { CommonModule } from '@angular/common';
+import { environment } from '../../environments/environment';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { PublicModule } from '../public/public.module';
 import { ProtectedModule } from '../protected/protected.module';
+
 
 @NgModule({
 	declarations: [],
 	imports: [
+		AngularFireModule.initializeApp(environment.firebase),
+		AngularFireAuthModule,
+		AngularFirestoreModule,
+		AngularFireStorageModule,
 		CommonModule,
 		PublicModule,
 		ProtectedModule
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+	production: false,
+	firebase: {
+		apiKey: '<your-key>',
+		authDomain: '<your-project-authdomain>',
+		databaseURL: '<your-database-URL>',
+		projectId: '<your-project-id>',
+		storageBucket: '<your-storage-bucket>',
+		messagingSenderId: '<your-messaging-sender-id>'
+	}
 };
 
 /*
