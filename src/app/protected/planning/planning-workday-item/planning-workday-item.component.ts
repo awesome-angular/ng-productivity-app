0765a3eae395733aa9792ca60f9bf374141cc900
Simon Dieny <simon.dieny@gmail.com>
2019-02-05 20:35:11 +0100

    feat(planning): add alias on input property in dumb component

--- src/app/protected/planning/planning-workday-item/planning-workday-item.component.ts
+++ src/app/protected/planning/planning-workday-item/planning-workday-item.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PlanningWorkdayItemComponent implements OnInit {
 
-	@Input() workday;
+	@Input('workday') currentWorkday;
 
 	constructor() { }
 
