270a11addf219a8bbe452b52b4b1201054805f64
Simon Dieny <simon.dieny@gmail.com>
2019-02-05 21:24:41 +0100

    feat(planning): intercept input property changes with ngOnChanges

--- src/app/protected/planning/planning-workday-item/planning-workday-item.component.ts
+++ src/app/protected/planning/planning-workday-item/planning-workday-item.component.ts
@@ -1,24 +1,42 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChange } from '@angular/core';
 
 @Component({
 	selector: 'al-planning-workday-item',
 	templateUrl: './planning-workday-item.component.html',
 	styles: []
 })
-export class PlanningWorkdayItemComponent {
+export class PlanningWorkdayItemComponent implements OnChanges {
 
-	private currentWorkday;
+	@Input() dueDate: string;
+	@Input() doneTasks: number | string;
+	@Input() remainingTasks: number | string;
 
-	@Input()
-	set workday(workday) {
-		this.currentWorkday = workday || {};
+	ngOnChanges(changes: { [propKey: string]: SimpleChange }) {
+		for (let propName in changes) {
+			this.update(propName, changes[propName].currentValue);
+		}
+	}
 
-		if ('Lundi' === workday.dueDate) {
-			this.currentWorkday.dueDate += ' (Aujourd\'hui)';
+	update(propName, propValue) {
+
+		switch (propName) {
+			case 'dueDate': {
+				if ('Lundi' === propValue) this.dueDate += ' (Aujourd\'hui)';
+				break;
+			}
+			case 'doneTasks': {
+				if (0 === propValue) this.doneTasks = 'Aucune tâche terminé.';
+				break;
+			}
+			case 'remainingTasks': {
+				if (0 === propValue) this.remainingTasks = 'Journée de travail terminée !';
+				break;
+			}
+			default: {
+				break;
+			}
 		}
 
 	}
-
-	get workday() { return this.workday; }
 
 }
