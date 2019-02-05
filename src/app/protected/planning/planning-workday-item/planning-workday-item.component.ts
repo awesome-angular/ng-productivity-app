314373631dcc622a6ca9deabc71999feba880f36
Simon Dieny <simon.dieny@gmail.com>
2019-02-05 20:49:46 +0100

    refactor(planning): remove useless ngoninit implementation in dumb component

--- src/app/protected/planning/planning-workday-item/planning-workday-item.component.ts
+++ src/app/protected/planning/planning-workday-item/planning-workday-item.component.ts
@@ -1,17 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
 	selector: 'al-planning-workday-item',
 	templateUrl: './planning-workday-item.component.html',
 	styles: []
 })
-export class PlanningWorkdayItemComponent implements OnInit {
+export class PlanningWorkdayItemComponent {
 
 	@Input('workday') currentWorkday;
-
-	constructor() { }
-
-	ngOnInit() {
-	}
 
 }
