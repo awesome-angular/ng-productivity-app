7e5eeaa52220cc422e44c290cecddbd68c67da59
Simon Dieny <simon.dieny@gmail.com>
2019-02-23 22:07:12 +0100

    feat(routing): add routes for page components

--- src/app/protected/protected-routing.module.ts
+++ src/app/protected/protected-routing.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { ParametersComponent } from './parameters/parameters/parameters.component';
 import { PlanningComponent } from './planning/planning/planning.component';
+import { ProfilComponent } from './profil/profil/profil.component';
+import { WorkdayComponent } from './workday/workday/workday.component';
 
 const routes: Routes = [
-	{ path: 'planning', component: PlanningComponent }
+	{
+		path: 'app',
+		children: [
+			{ path: 'dashboard', component: DashboardComponent },
+			{ path: 'parameters', component: ParametersComponent },
+			{ path: 'planning', component: PlanningComponent },
+			{ path: 'profil', component: ProfilComponent },
+			{ path: 'workday', component: WorkdayComponent },
+		]
+	}
 ];
 
 @NgModule({
--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
+import { LoginComponent } from './login/login/login.component';
+import { RegisterComponent } from './register/register/register.component';
 
 const routes: Routes = [
-	{ path: 'home', component: HomeComponent }
+	{ path: 'home', component: HomeComponent },
+	{ path: 'login', component: LoginComponent },
+	{ path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
