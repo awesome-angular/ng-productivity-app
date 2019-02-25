db2d44eef795a751f3b86d7905b204275d501156
Simon Dieny <simon.dieny@gmail.com>
2019-02-25 11:21:14 +0100

    feat(routing): add redirections in navbar component

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,14 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'al-navbar',
-	templateUrl: './navbar.component.html'
+	templateUrl: './navbar.component.html',
+	styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
 
-	constructor() { }
+	private homePath: string = 'home';
+	private loginPath: string = 'login';
+	private registerPath: string = 'register';
+
+	constructor(private router: Router) { }
 
 	ngOnInit() {
 	}
+
+	public isActive(page: string): boolean {
+		return this.router.isActive(page, true);
+	}
+
+	public navigate(page: string): void {
+		this.router.navigate([page]);
+	}
+
 
 }
