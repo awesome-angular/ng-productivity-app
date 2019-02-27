7c7765e00f2f5b4c90f5d3a1eed1e423bf1bc4b2
Simon Dieny <simon.dieny@gmail.com>
2019-02-27 13:58:34 +0100

    feat(login): replace common module by shared module

--- src/app/public/login/login.module.ts
+++ src/app/public/login/login.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { LoginComponent } from './login/login.component';
 import { LoginRoutingModule } from './login-routing.module';
 
 @NgModule({
 	declarations: [LoginComponent],
 	imports: [
-		CommonModule,
+		SharedModule,
 		LoginRoutingModule
 	]
 })
