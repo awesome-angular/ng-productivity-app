5ab46fa205c4c1881fa3b28fc5bb26c1c438ed1b
Simon Dieny <simon.dieny@gmail.com>
2019-02-28 16:56:54 +0100

    feat(forms): integration of workday form in page component, and add subcomponents

--- src/app/protected/workday/workday-form/workday-form.component.ts
+++ src/app/protected/workday/workday-form/workday-form.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'al-workday-form',
-  templateUrl: './workday-form.component.html',
-  styles: []
+	selector: 'al-workday-form',
+	templateUrl: './workday-form.component.html',
+	styles: []
 })
 export class WorkdayFormComponent implements OnInit {
 
-  constructor() { }
+	private workdayForm: FormGroup;
 
-  ngOnInit() {
-  }
+	constructor(private fb: FormBuilder) { }
+
+	ngOnInit() {
+		this.workdayForm = this.createWorkdayForm();
+	}
+
+	get dueDate() { return this.workdayForm.get('dueDate') }
+	get notes() { return this.workdayForm.get('notes') }
+	get tasks() { return this.workdayForm.get('tasks') as FormArray; }
+
+	createWorkdayForm(): FormGroup {
+		return this.fb.group({
+			'dueDate': '',
+			'tasks': this.fb.array([]),
+			'notes': ''
+		});
+	}
+
+	submit() {
+		console.info(this.workdayForm.value);
+	}
 
 }
