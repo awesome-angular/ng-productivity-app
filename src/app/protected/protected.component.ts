0268df61d70f53d54bb7cb9d8bb74a90e592c849
Simon Dieny <simon.dieny@gmail.com>
2019-05-28 23:58:10 +0200

    fix(services): use subscription instead of async pipe for sidenav

--- src/app/protected/protected.component.ts
+++ src/app/protected/protected.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { LayoutService } from 'src/app/core/services/layout.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
 	selector: 'al-protected',
 	templateUrl: './protected.component.html',
 	styleUrls: ['./protected.component.scss']
 })
-export class ProtectedComponent implements OnInit {
+export class ProtectedComponent implements OnInit, OnDestroy {
 
-	public isSidenavCollapsed$: Observable<boolean>;
+	public isSidenavCollapsed: boolean;
+	private subscription: Subscription;
 
 	constructor(private layoutService: LayoutService) { }
 
 	ngOnInit() {
-		this.isSidenavCollapsed$ = this.layoutService.isSidenavCollapsed$;
+		this.subscription = this.layoutService.isSidenavCollapsed$
+			.subscribe(isSidenavCollapsed => this.isSidenavCollapsed = isSidenavCollapsed);
+	}
+
+	ngOnDestroy() {
+		this.subscription.unsubscribe();
 	}
 
 }
