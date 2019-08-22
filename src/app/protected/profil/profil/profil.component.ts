7c5101402a611ff3381b7bf3a15bc2972a0a4579
Simon Dieny <simon.dieny@gmail.com>
2019-08-22 14:38:23 +0200

    feat(profil): add profil form

--- src/app/protected/profil/profil/profil.component.ts
+++ src/app/protected/profil/profil/profil.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { User } from 'src/app/shared/models/user';
 
 @Component({
   selector: 'al-profil',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilComponent implements OnInit {
 
-  constructor() { }
+  profilForm: FormGroup;
+  user: User;
+
+  constructor(
+    public fb: FormBuilder,
+    public authService: AuthService) { }
 
   ngOnInit() {
+    this.user = this.authService.currentUser;
+
+    this.profilForm = this.fb.group({
+      'name': [this.user.name, [
+				Validators.required,
+				Validators.minLength(4),
+				Validators.maxLength(20),
+				Validators.pattern('^[a-zA-Z0-9_-]*$')
+			]],
+      'avatar': [this.user.avatar, [
+        Validators.pattern('https?://.+')
+      ]]
+    })
+  }
+
+  get name() { return this.profilForm.get('name'); }
+	get avatar() { return this.profilForm.get('avatar'); }
+
+  submit() {
+    this.user.name = this.name.value;
+    this.user.avatar = this.avatar.value;
+    this.authService.updateUserState(this.user).subscribe();
   }
 
 }
