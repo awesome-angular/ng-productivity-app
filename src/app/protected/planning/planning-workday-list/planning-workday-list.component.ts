f18063d049e72df1e61df309f163d2bd83361321
Simon <simon.dieny@gmail.com>
2020-04-09 11:50:28 +0200

    step(console.log workday in planning page)

--- src/app/protected/planning/planning-workday-list/planning-workday-list.component.ts
+++ src/app/protected/planning/planning-workday-list/planning-workday-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WorkdaysService } from 'src/app/core/services/workdays.service';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
 	selector: 'al-planning-workday-list',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlanningWorkdayListComponent implements OnInit {
 
-	constructor() { }
+	constructor(
+		private authService: AuthService,
+		private workdaySerice: WorkdaysService) { }
 
-	ngOnInit() {}
+	ngOnInit() {
+		const id: string = this.authService.currentUser.id;
+		this.workdaySerice.getWorkdayByUser(id).subscribe(workdays => {
+			console.info(workdays);
+		});
+	}
 
 	onWorkdayRemoved(dueDate: string) {
 		console.info(dueDate);
