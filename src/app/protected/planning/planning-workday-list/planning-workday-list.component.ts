f1c608671f919a893f3f0db980283ba73fec3c5f
Simon <simon.dieny@gmail.com>
2020-04-09 10:14:43 +0200

    step(reset planning component)

--- src/app/protected/planning/planning-workday-list/planning-workday-list.component.ts
+++ src/app/protected/planning/planning-workday-list/planning-workday-list.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import { of } from 'rxjs';
-import { delay } from 'rxjs/operators';
-
 @Component({
 	selector: 'al-planning-workday-list',
 	templateUrl: './planning-workday-list.component.html',
@@ -10,25 +7,12 @@ import { delay } from 'rxjs/operators';
 })
 export class PlanningWorkdayListComponent implements OnInit {
 
-	public workdays$;
-	public workdays;
-
 	constructor() { }
 
-	ngOnInit() {
-		this.workdays = [
-			{ dueDate: 'Lundi', doneTasks: 1, remainingTasks: 0 },
-			{ dueDate: 'Mardi', doneTasks: 0, remainingTasks: 2 },
-			{ dueDate: 'Mercredi', doneTasks: 0, remainingTasks: 1 }
-		];
-
-		this.workdays$ = of(this.workdays).pipe(delay(1000));
-
-	}
+	ngOnInit() {}
 
 	onWorkdayRemoved(dueDate: string) {
-		this.workdays = this.workdays.filter(workday => !dueDate.includes(workday.dueDate));
-		this.workdays$ = of(this.workdays);
+		console.info(dueDate);
 	}
 
 }
