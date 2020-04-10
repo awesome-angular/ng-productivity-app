9ea6683eebe8c14865815b1924bdcd043cf60a44
Simon <simon.dieny@gmail.com>
2020-04-10 17:49:07 +0200

    step(redirect to workday page with query parameter)

--- src/app/protected/planning/planning-workday-item/planning-workday-item.component.ts
+++ src/app/protected/planning/planning-workday-item/planning-workday-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 import { Workday } from 'src/app/shared/models/workday';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'al-planning-workday-item',
@@ -11,10 +12,23 @@ export class PlanningWorkdayItemComponent implements OnInit {
 	@Input() workday: Workday;
 	@Output() workdayRemoved = new EventEmitter<string>();
 
+	constructor(private router: Router) {}
+
 	ngOnInit() {}
 
 	removeWorkday(displayDate: string) {
 		this.workdayRemoved.emit(displayDate);
+	}
+
+	goWorkday(workday: Workday) {
+		this.router.navigate(
+			['app/workday'], 
+			{ 
+				queryParams: { 
+					date: workday.dueDate 
+				} 
+			}
+		);
 	}
 
 }
