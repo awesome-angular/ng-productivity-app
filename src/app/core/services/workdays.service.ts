de06513e9984f9041d3235f820bc75a17aef6b1c
Simon <simon.dieny@gmail.com>
2020-05-15 14:19:29 +0200

    add removeWorkday method

--- src/app/core/services/workdays.service.ts
+++ src/app/core/services/workdays.service.ts
@@ -66,6 +66,26 @@ export class WorkdaysService {
     );
   }
 
+  remove(workday: Workday) {
+    const url = `${environment.firebase.firestore.baseURL}/workdays/${workday.id}?key=${environment.firebase.apiKey}`;
+    const jwt: string = localStorage.getItem('token');
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': `Bearer ${jwt}`
+      })
+    };
+
+    return this.http.delete(url, httpOptions).pipe(
+      tap(_ => this.toastrService.showToastr({
+        category: 'success',
+        message: 'Votre journée de travail a été supprimé avec succès.'
+      })),
+      catchError(error => this.errorService.handleError(error)),
+      finalize(() => this.loaderService.setLoading(false))
+    );
+  }
+
   /**
    * Return Firestore's workday.
    * 
