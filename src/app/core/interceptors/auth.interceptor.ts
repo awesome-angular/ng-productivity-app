6644d811366be7eb99db953311ff1ffbce004303
Simon Dieny <simon.dieny@gmail.com>
2020-05-20 11:54:59 +0200

    step(add content type on each request and remove console.log)

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(request).pipe(
-      tap(_ => console.info('interceptor !'))
-    );
+    request = this.addContentType(request);
+
+    return next.handle(request);
+  }
+
+  private addContentType(request: HttpRequest<any>) {
+    return request.clone({
+      setHeaders: {
+        'Content-Type': 'application/json'
+      }
+    });    
   }
 }
