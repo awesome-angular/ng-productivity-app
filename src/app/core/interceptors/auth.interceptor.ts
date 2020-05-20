a8eca329f372b9f5020a20f867a0dce845909300
Simon Dieny <simon.dieny@gmail.com>
2020-05-20 12:49:02 +0200

    step(add jwt on request who need it)

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -6,16 +6,32 @@ import { Observable } from 'rxjs';
 export class AuthInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (!this.isPublicRequest(request.url)) {
+      request = this.addToken(request, localStorage.getItem('token'));
+    }
+
     request = this.addContentType(request);
 
     return next.handle(request);
   }
 
-  private addContentType(request: HttpRequest<any>) {
+  private addContentType(request: HttpRequest<any>): HttpRequest<any> {
     return request.clone({
       setHeaders: {
         'Content-Type': 'application/json'
       }
     });    
+  }
+
+  private addToken(request: HttpRequest<any>, token: string): HttpRequest<any> {
+    return request.clone({
+      setHeaders: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
+  }
+
+  private isPublicRequest(url: string): boolean {
+    return (url.includes('verifyPassword') || url.includes('signupNewUser'));
   }
 }
