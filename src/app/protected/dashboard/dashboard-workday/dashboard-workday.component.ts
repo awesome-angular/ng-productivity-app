7a3994c8c12e6e71b96ada4a89fb6dd8617fa3b5
Simon Dieny <simon.dieny@gmail.com>
2020-05-29 11:53:46 +0200

    is workday empty ?

---
+++ src/app/protected/dashboard/dashboard-workday/dashboard-workday.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Workday } from 'src/app/shared/models/workday';
+
+@Component({
+  selector: 'al-dashboard-workday',
+  templateUrl: './dashboard-workday.component.html',
+  styleUrls: ['./dashboard-workday.component.scss']
+})
+export class DashboardWorkdayComponent implements OnInit {
+
+  @Input() workday: Workday;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/protected/dashboard/dashboard.module.ts
+++ src/app/protected/dashboard/dashboard.module.ts
@@ -4,9 +4,10 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DashboardPomodoroProgressComponent } from './dashboard-pomodoro-progress/dashboard-pomodoro-progress.component';
 import { DashboardTaskItemComponent } from './dashboard-task-item/dashboard-task-item.component';
+import { DashboardWorkdayComponent } from './dashboard-workday/dashboard-workday.component';
 
 @NgModule({
-	declarations: [DashboardComponent, DashboardPomodoroProgressComponent, DashboardTaskItemComponent],
+	declarations: [DashboardComponent, DashboardPomodoroProgressComponent, DashboardTaskItemComponent, DashboardWorkdayComponent],
 	imports: [
 		SharedModule,
 		DashboardRoutingModule
