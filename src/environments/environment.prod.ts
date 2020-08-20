e4b59f196f1e0516322716b5c8d7e0f21f0ee258
Simon Dieny <simon.dieny@gmail.com>
2020-08-20 10:24:49 +0200

    step : setup env variables for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
 export const environment = {
-	production: true
+	production: true,
+	firebase: {
+		apiKey: 'AIzaSyAkgLyl4GXcHXe1Y7NhjPpntXVQ0fCsOuc',
+		authDomain: 'ng-awesome-list.firebaseapp.com',
+		databaseURL: 'https://ng-awesome-list.firebaseio.com',
+		projectId: 'ng-awesome-list',
+		storageBucket: 'ng-awesome-list.appspot.com',
+		messagingSenderId: '1039499474549',
+		auth: {
+			baseURL: 'https://www.googleapis.com/identitytoolkit/v3/relyingparty'
+		},
+		firestore: {
+			baseURL : 'https://firestore.googleapis.com/v1/projects/ng-awesome-list/databases/(default)/documents'
+		}
+	}
 };
